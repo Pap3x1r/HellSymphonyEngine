082d33ce1f6291443c9df58ab82172938e7f0123
Pap3x1r <122349397+Pap3x1r@users.noreply.github.com>
2025-04-04 18:19:09 +0700

    QTE fixed + Branch merged

--- NextEngine/QTEManager.cpp
+++ NextEngine/QTEManager.cpp
@@ -1,52 +1,84 @@
-#include "QTEManager.h"
+    #include "QTEManager.h"
 
-QTEManager* QTEManager::instance = nullptr;
+    QTEManager* QTEManager::instance = nullptr;
 
-void QTEManager::startQTE(const vector<char>& sequence, float timeLimit) {
-    this->sequence = sequence;
-    this->timeLimit = timeLimit;
-    currentIndex = 0;
-    inputs.clear();
-    startTime = chrono::steady_clock::now();
-    isActive = true;
-}
-
-void QTEManager::handleInput(char key) {
-    if (!isActive) {
-        return;
+    void QTEManager::startQTE(const vector<char>& sequence, float timeLimit) {
+        this->sequence = sequence;
+        this->timeLimit = timeLimit;
+        currentIndex = 0;
+        inputs.clear();
+        startTime = chrono::steady_clock::now();
+        isActive = true;
     }
 
-    inputs.push_back(key);
-    if (inputs.size() <= sequence.size()) {
-        if (key == sequence[inputs.size() - 1]) {
-            if (inputs.size() == sequence.size()) {
-                success();
-            }
+    void QTEManager::handleInput(char key) {
+        if (!isActive) {
+            return;
         }
-        else {
+
+        if (key == ' ') {
+            return;
+        }
+
+        inputs.push_back(key);
+
+        if (key != sequence[currentIndex]) {
+            fail();
+            return;
+        }
+
+        currentIndex++;
+
+        if (currentIndex == sequence.size()) {
+            success();
+        }
+    }
+
+    void QTEManager::update() {
+        if (!isActive) {
+            return;
+        }
+
+        auto currentTime = chrono::steady_clock::now();
+        auto elapsedTime = chrono::duration_cast<chrono::seconds>(currentTime - startTime).count();
+        if (elapsedTime > timeLimit) {
             fail();
         }
     }
-}
 
-void QTEManager::update() {
-    if (!isActive) {
-        return;
+    void QTEManager::success() {
+        cout << "QTE succeeded!" << endl;
+
+        cout << "Sequence: ";
+        for (char key : sequence) {
+            cout << key << " ";
+        }
+        cout << endl;
+
+        cout << "Input: ";
+        for (char key : inputs) {
+            cout << key << " ";
+        }
+
+        isActive = false;
     }
 
-    auto currentTime = chrono::steady_clock::now();
-    auto elapsedTime = chrono::duration_cast<chrono::seconds>(currentTime - startTime).count();
-    if (elapsedTime > timeLimit) {
-        fail();
+    void QTEManager::fail() {
+        cout << "QTE failed!" << endl;
+
+
+        cout << "Sequence: ";
+        for (char key : sequence) {
+            cout << key << " ";
+        }
+        cout << endl;
+
+        cout << "Input: ";
+        for (char key : inputs) {
+            cout << key << " ";
+        }
+
+
+        cout << endl;
+        isActive = false;
     }
-}
-
-void QTEManager::success() {
-    cout << "QTE succeeded!" << endl;
-    isActive = false;
-}
-
-void QTEManager::fail() {
-    cout << "QTE failed!" << endl;
-    isActive = false;
-}
